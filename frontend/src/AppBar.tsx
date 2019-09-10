fb8a09b4c39e02b80b9e70ab761185caa24e0c33
Christopher Dobler <chrisdobler@gmail.com>
2019-09-10 00:50:09 -0700

    Adds chest icon

--- frontend/src/AppBar.tsx
+++ frontend/src/AppBar.tsx
@@ -182,7 +182,7 @@ export default function PrimarySearchAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-            Material-UI
+            Chest
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
