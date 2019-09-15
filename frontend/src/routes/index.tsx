5e202ecf1e3e03d8d2ae7cee82eac92790a8cfb0
Christopher Dobler <chrisdobler@gmail.com>
2019-09-14 23:56:49 -0700

    fixes the routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -8,8 +8,16 @@ import { Route, Switch, Link, Redirect } from 'react-router-dom';
 const Routes = props => (
   <Switch>
     <Route path="/" component={ListContainer}>
-      <Route path="/items" component={ListContainer}>
-        <Route path="/items/add" component={ItemEditContainer} />
+      <Route
+        path="/items"
+        render={({ match: { url } }) => (
+          <>
+            <Route path={`${url}/`} component={ListContainer} exact />
+            <Route path={`${url}/add`} component={ItemEditContainer} />
+          </>
+        )}
+      >
+        {/* <Route path="/items/add" component={ItemEditContainer} /> */}
         {/* <Route path="*" component={NotFound} /> */}
       </Route>
     </Route>
