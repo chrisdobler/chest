76f2ad7086f8ac9c52180229b5c9f7c7a0b6258a
Christopher Dobler <chrisdobler@gmail.com>
2022-01-19 20:41:32 -0800

    update type

---
+++ frontend/src/types/item.d.ts
@@ -0,0 +1,19 @@
+interface Photo {
+    id: string;
+    url: string;
+}
+
+interface Container {
+    id: string;
+    name: string;
+}
+
+interface Tag {}
+
+export interface Item {
+    id: string;
+    photos: Array<Photo>;
+    location: Container;
+    name?: string;
+    tags?: Array<Tag>;
+}
