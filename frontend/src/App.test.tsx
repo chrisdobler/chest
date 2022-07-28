fb86a3d188aef41ba9a8939218954a4d352f3eb1
Christopher Dobler <chrisdobler@gmail.com>
2022-07-27 17:53:00 -0700

    update app test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import Store from './store';
+
+const StoreInstance = Store();
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <Provider store={StoreInstance}>
+            <App />
+        </Provider>,
+        div
+    );
+    ReactDOM.unmountComponentAtNode(div);
 });
