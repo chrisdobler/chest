b02622a6b6a3cdba92ee80fe4082dce7f9ee61f8
Christopher Dobler <chrisdobler@gmail.com>
2023-01-24 00:07:57 -0800

    fixes deploy issues

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,6 +3,6 @@
 git init
 git add . --all
 git commit -m "dokku deploy"
-git remote add dokku dokku@dokku3.br4incandy.com:chest-webapp
-git push --force dokku master
+git remote add dokku dokku@dokku4.dobsys.io:chest-webapp
+git push --force dokku main
 rm -rf .git
--- server/deploy.sh
+++ server/deploy.sh
@@ -4,5 +4,5 @@ git init
 git add . --all
 git commit -m "dokku deploy"
 git remote add dokku dokku@dokku4.dobsys.io:chest-api
-git push --force dokku master
-rm -rf .git
+git push --force dokku main
+rm -rf .git 
